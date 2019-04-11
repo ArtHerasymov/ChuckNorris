fa2179920357ca531871e783d570da4c82779c03
ArtHerasymov <artieherasymov@gmail.com>
2019-04-11 21:04:23 +0300

    Search field and submit button added

--- ChuckNorrisApp/ChuckNorrisApp/screens/Home.js
+++ ChuckNorrisApp/ChuckNorrisApp/screens/Home.js
@@ -6,12 +6,18 @@ import {
   View,
   Button
 } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 class Home extends React.Component {
  
+  constructor() {
+    super();
+    this.state = { query: 'Search...' }
+  }
+
    static navigationOptions = {
     title: 'Home',
-   headerStyle: {
+    headerStyle: {
     backgroundColor: '#03A9F4',
     },
     headerTintColor: '#fff',
@@ -20,11 +26,26 @@ class Home extends React.Component {
     },
  
   }; 
+
+  onPressSearch = () => {
+    // Making HTTP request and going to another activity
+  }
  
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.headerText} >Home Activity</Text>
+        <TextInput 
+          onChangeText = {(query) => this.setState({query})}
+          style ={{height: 40,width: 300, borderColor: 'gray', borderWidth: 1}}
+          value = { this.state.query }
+        />
+        <Button
+          title="Search"
+          style ={{float: 'right'}}
+          color="#841584"
+          onPress= {this.onPressSearch}
+        />
         <Button
           title="Go to Profile Activity"
           onPress={() => this.props.navigation.navigate('SearchResults')}
